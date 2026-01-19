52536dcbe345f6176a2fb449467566642845f808
bokuseiryu <boku@g-links.co.jp>
2026-01-19 10:13:02 +0900

    fix: revert to environment variables for database connection

--- backend/config/db.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  MONGO_URI: 'mongodb+srv://tempuser:LFgRuKZqDKrxXciw@cluster0.6kobioq.mongodb.net/hotel-data?retryWrites=true&w=majority'
-};
--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -4,11 +4,11 @@
 // ==================================================================
 
 const mongoose = require('mongoose');
-const { MONGO_URI } = require('../config/db.config');
+require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
